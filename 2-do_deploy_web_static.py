6697023264bbb2f60b2ffd090722be658162bba5
BrahimElhajji <elhajji080898@gmail.com>
2024-05-03 19:41:38 -0700

    Deploy archive! fixing

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,13 +4,17 @@ Fabric script (based on the file 1-pack_web_static.py)
 that distributes an archive to your web servers,
 using the function do_deploy
 """
+
 from fabric.api import env, put, run
 import os
 
-env.hosts = ['web-01.brahimelhajji.tech', 'web-02.brahimelhajji.tech']
+env.hosts = ['100.26.246.41', '54.166.60.69']
 
 
 def do_deploy(archive_path):
+    """creates and distributes an archive to your web servers,
+    using the function deploy"""
+
     if not os.path.exists(archive_path):
         return False
 
