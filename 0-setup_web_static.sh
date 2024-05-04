a93c341e56f4984e4d281af8d2be77b67753898f
BrahimElhajji <elhajji080898@gmail.com>
2024-05-03 17:05:00 -0700

    fixing task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,31 +1,36 @@
 #!/usr/bin/env bash
 
+# Ensure script is executed with sudo privileges
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root" 
+    exit 1
+fi
+
 # Install Nginx if it's not already installed
-sudo apt-get update
-sudo apt-get install -y nginx
-sudo ufw allow 'Nginx HTTP'
+apt-get update
+apt-get install -y nginx
+ufw allow 'Nginx HTTP'
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/releases/test
-sudo mkdir -p /data/web_static/shared
-sudo touch /data/web_static/releases/test/index.html
+# Create necessary folders if they don't exist
+mkdir -p /data/web_static/{releases/test,shared}
 
-sudo echo "<html>
+# Create a fake HTML file for testing
+echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+</html>" | tee /data/web_static/releases/test/index.html > /dev/null
 
-sudo rm -f /etc/nginx/sites-enabled/web_static
-sudo ln -s /data/web_static/releases/test /data/web_static/current
+# Create symbolic link
+ln -sf /data/web_static/releases/test /data/web_static/current
 
-sudo chown -R ubuntu:ubuntu /data
+# Give ownership of /data/ folder to ubuntu user and group
+chown -R ubuntu:ubuntu /data
 
-sudo echo "server {
+# Update Nginx configuration
+echo "server {
     listen 80;
     server_name _;
     location /hbnb_static {
@@ -33,6 +38,8 @@ sudo echo "server {
     }
 }" > /etc/nginx/sites-available/web_static
 
-sudo ln -s /etc/nginx/sites-available/web_static /etc/nginx/sites-enabled/
+# Enable the Nginx configuration by creating a symbolic link
+ln -sf /etc/nginx/sites-available/web_static /etc/nginx/sites-enabled/
 
-sudo service nginx restart
+# Restart Nginx
+service nginx restart
