cd0f00a35ffb1e699f14e1ff41bdde6797b11e2b
BrahimElhajji <elhajji080898@gmail.com>
2024-05-19 15:45:16 -0700

    task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Import Flask for runing the web app"""
+from flask import Flask, render_template
+from models import storage
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from flask import request, abort
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db_session(exception):
+    """teardown the database session after each request"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """route to display the list of states"""
+    states = storage.all("State")
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
