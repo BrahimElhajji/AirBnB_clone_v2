5f6a9cdd0ade862a8500044106340cb6504b8dee
BrahimElhajji <elhajji080898@gmail.com>
2024-05-19 16:09:07 -0700

    task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,17 +2,9 @@
 """Import Flask for runing the web app"""
 from flask import Flask, render_template
 from models import storage
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-from flask import request, abort
+from models.state import State
 
 app = Flask(__name__)
-
-
-engine = create_engine('sqlite:///storage.db')
-db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
 
 
 @app.teardown_appcontext
