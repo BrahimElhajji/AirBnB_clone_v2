4549e261524d8fa880006336613fc0f5e1837d88
BrahimElhajji <elhajji080898@gmail.com>
2024-05-18 17:59:38 -0700

    fixing task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,10 +4,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """fun that display Hello HBNB!"""
-    return "<h1>Hello HBNB!</h1>"
+    return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
