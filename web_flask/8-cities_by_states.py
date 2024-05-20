4d6481351d2eff6c2d2d995a74d130f5942b49ab
BrahimElhajji <elhajji080898@gmail.com>
2024-05-19 17:18:22 -0700

    task 9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Import Flask for runing the web app"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Displays a html page with states and cities"""
+    states = storage.all('State')
+    sorted_states = sorted(states, key=lambda x: x.name)
+    return render_template('states.html', states=sorted_states)
+
+
+@app.teardown_appcontext
+def close_storage(exc):
+    """close the session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
