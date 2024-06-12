464494abacc0a0fc4e53e2e4e823eb7c0f6bcaae
BrahimElhajji <elhajji080898@gmail.com>
2024-06-12 12:45:50 -0700

    test

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""starting a Flask web application"""
+""" Start a Flask web application """
 from flask import Flask
 
 app = Flask(__name__)
 
-
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """fun that display Hello HBNB!"""
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hello():
     return "Hello HBNB!"
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
